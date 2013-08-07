70b2502bc122e2ab15710839efca7faf3e7e189d
Frizz925 <Frizz925@hotmail.com>
2013-08-07 06:29:09 +0100

    Some changes

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('TOKENS', 'tokens.scrt');
-define('OSUAPI_KEY', '3ba676f93b2d8fa6ecb2b857fefa4530a21fd50d');
-define('CONSUMER_KEY', 'sIaGcAI6wVYipOX1ZWoA');
-define('CONSUMER_SECRET', '4QZgGNHIfgp9CaGlzfPwLTQ4py4cmY8ofv41ppbAPY');
-define('OAUTH_CALLBACK', 'http://autopro.p.ht/osuautorecent/complete.php');
-
-$usercount = count(file(TOKENS));
-?>
--- remove/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('TOKENS', 'tokens.scrt');
-define('CONSUMER_KEY', 'sIaGcAI6wVYipOX1ZWoA');
-define('CONSUMER_SECRET', '4QZgGNHIfgp9CaGlzfPwLTQ4py4cmY8ofv41ppbAPY');
-define('OAUTH_CALLBACK', 'http://autopro.p.ht/osuautorecent/remove/complete.php');
-?>
